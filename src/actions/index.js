4d9bf5da069373f0d0b56c833d4ba091999098aa
Darrell Klein <dkleined92@gmail.com>
2018-09-06 17:25:35 -0700

    added axios and began fetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = "e658178f1c6fefa447d22a290447dfee";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
     return {
         type: FETCH_WEATHER,
-        
+        payload: request
     };
 }
